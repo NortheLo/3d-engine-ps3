655d429bdf8580614c92a91b0672f1d892a73df7
NortheLo <louis@northe-online.de>
2023-12-09 01:16:06 +0100

    Added gitignore

---
+++ build/main.d
@@ -0,0 +1,82 @@
+main.o: /home/louis/eclipse-workspace/3d-engine-shaders/source/main.cpp \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/ppu-types.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/sys/process.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/ppu-lv2.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/lv2/process.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/lv2/syscalls.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/io/pad.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/rsx.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/mm.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/gcm_sys.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/rsx_program.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/commands.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/rsx_function_macros.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/commands_inc.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/sysutil/sysutil.h \
+ /home/louis/eclipse-workspace/3d-engine-shaders/include/acid.h \
+ /home/louis/eclipse-workspace/3d-engine-shaders/include/mesh.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/vectormath_aos.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/vecidx_aos.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/floatInVec.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/simdmath/simdmath.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/boolInVec.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/vec_aos.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/quat_aos.h \
+ /home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/mat_aos.h \
+ /home/louis/eclipse-workspace/3d-engine-shaders/include/rsxutil.h \
+ /home/louis/eclipse-workspace/3d-engine-shaders/build/diffuse_specular_shader_vpo.h \
+ /home/louis/eclipse-workspace/3d-engine-shaders/build/diffuse_specular_shader_fpo.h
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/ppu-types.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/sys/process.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/ppu-lv2.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/lv2/process.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/lv2/syscalls.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/io/pad.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/rsx.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/mm.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/gcm_sys.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/rsx_program.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/commands.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/rsx_function_macros.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/rsx/commands_inc.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/sysutil/sysutil.h:
+
+/home/louis/eclipse-workspace/3d-engine-shaders/include/acid.h:
+
+/home/louis/eclipse-workspace/3d-engine-shaders/include/mesh.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/vectormath_aos.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/vecidx_aos.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/floatInVec.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/simdmath/simdmath.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/boolInVec.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/vec_aos.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/quat_aos.h:
+
+/home/louis/Documents/ps3/ps3toolchain/bin/ppu/include/vectormath/cpp/mat_aos.h:
+
+/home/louis/eclipse-workspace/3d-engine-shaders/include/rsxutil.h:
+
+/home/louis/eclipse-workspace/3d-engine-shaders/build/diffuse_specular_shader_vpo.h:
+
+/home/louis/eclipse-workspace/3d-engine-shaders/build/diffuse_specular_shader_fpo.h:
