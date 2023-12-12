1e48de35b4912f5694096c9aeeb6faf0c4e9a5aa
NortheLo <louis@northe-online.de>
2023-12-12 20:36:12 +0100

    Fixed and simplified camera movement

--- build/diffuse_specular_shader_fpo.h
+++
@@ -1,3 +0,0 @@
-extern const u8 diffuse_specular_shader_fpo_end[];
-extern const u8 diffuse_specular_shader_fpo[];
-extern const u32 diffuse_specular_shader_fpo_size;
--- build/diffuse_specular_shader_vpo.h
+++
@@ -1,3 +0,0 @@
-extern const u8 diffuse_specular_shader_vpo_end[];
-extern const u8 diffuse_specular_shader_vpo[];
-extern const u32 diffuse_specular_shader_vpo_size;
